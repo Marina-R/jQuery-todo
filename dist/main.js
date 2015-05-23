1a734697defe7d230319b4a6c0389f4f8f77dad7
maryna <luv_this_world@hotmail.com>
2015-05-23 17:25:31 -0500

    normal mode

---
+++ dist/main.js
@@ -0,0 +1,39 @@
+$(document).on('ready', start);
+function start(){
+	// localStorage.clear();
+    
+	var $submit = $('#input-form');
+	var $input = $('#form-line');
+	var $list = $('#todo-list');
+	var todoArray=[];
+
+	if (localStorage.getItem('list') === null) {
+		todoArray = [];
+	} else {
+		todoArray = JSON.parse(localStorage.getItem('list'));
+	}
+	render(todoArray);
+
+	var innerHtml = render(todoArray);
+	$list.html(innerHtml);
+
+	function onPost (e) {
+		e.preventDefault();
+		todoArray.push($input.val());
+		$input.val('');
+		var innerHtml = render(todoArray);
+		$list.html(innerHtml);
+		storage(todoArray);
+	}
+
+	function render(todo) {
+		return '<ul><li>' + todoArray.join('</li><li>') + '</li></ul>';
+	}
+
+	$submit.on('submit', onPost);
+    $('ul li').on('click', function (e) {e.target.style.textDecoration = 'line-through'});
+    
+    function storage () {
+		localStorage.setItem('list', JSON.stringify(elem));
+	}
+}
